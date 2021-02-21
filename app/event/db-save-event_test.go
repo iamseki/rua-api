d1e879a1834189ce4e534fa03f34fc0c92c2b762
iamseki <chrisyuri_19@hotmail.com>
2021-02-21 00:48:08 -0300

    test: add event save usecase

---
+++ app/event/db-save-event_test.go
@@ -0,0 +1,65 @@
+package eventusecases
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/iamseki/rua-api/domain"
+)
+
+type fakeRepository struct {
+	SaveFn func(m domain.EventModel) error
+}
+
+func (fr *fakeRepository) Save(m domain.EventModel) error {
+	return fr.SaveFn(m)
+}
+
+func makeEventModel() domain.EventModel {
+	return domain.EventModel{
+		Artist: domain.Artist{
+			User: domain.User{
+				Name:     "any-name",
+				LastName: "any-last-name",
+				Email:    "any-email@mail.com",
+				Password: "any-password",
+			},
+			PIX: "12345678",
+		},
+		Event: domain.Event{
+			Title:           "any-event",
+			Date:            time.Now(),
+			Place:           "any-place",
+			PlaceAnnotation: "near-any",
+			Category:        "any",
+			KeyWords:        []string{"any-1", "any-2"},
+		},
+	}
+}
+
+func TestSaveEventSucceed(t *testing.T) {
+	e := makeEventModel()
+	r := &fakeRepository{SaveFn: func(m domain.EventModel) error {
+		return nil
+	}}
+
+	sut := NewDbSaveEvent(r)
+	err := sut.Save(e)
+	if err != nil {
+		t.Errorf("Error on save event into repository: %v\n", err)
+	}
+}
+
+func TestSaveEventError(t *testing.T) {
+	e := makeEventModel()
+	r := &fakeRepository{SaveFn: func(m domain.EventModel) error {
+		return errors.New("fake error")
+	}}
+
+	sut := NewDbSaveEvent(r)
+	err := sut.Save(e)
+	if err == nil {
+		t.Error("Expect to be an error with fake error message")
+	}
+}
