323a8e9a52196d0ac21d97630eaba3dfdeb7af93
iamseki <chrisyuri_19@hotmail.com>
2021-02-21 23:11:21 -0300

    test: add find event usecase

---
+++ app/event/db-find-event_test.go
@@ -0,0 +1,44 @@
+package eventusecases
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/iamseki/rua-api/domain"
+)
+
+type fakeFindRepository struct {
+	FindFn func(o domain.EventFinderOptions) (domain.Event, error)
+}
+
+func (fr *fakeFindRepository) Find(o domain.EventFinderOptions) (domain.Event, error) {
+	return fr.FindFn(o)
+}
+
+func makeEventFinderOptions() domain.EventFinderOptions {
+	return domain.EventFinderOptions{
+		Title: "any-event",
+	}
+}
+
+func TestFindEventSucceed(t *testing.T) {
+	r := &fakeFindRepository{FindFn: func(o domain.EventFinderOptions) (domain.Event, error) {
+		return domain.Event{}, nil
+	}}
+	sut := NewDbFindEvent(r)
+	_, err := sut.Find(makeEventFinderOptions())
+	if err != nil {
+		t.Fatalf("Error on find event : %v\n", err)
+	}
+}
+
+func TestFindEventError(t *testing.T) {
+	r := &fakeFindRepository{FindFn: func(o domain.EventFinderOptions) (domain.Event, error) {
+		return domain.Event{}, errors.New("fake error")
+	}}
+	sut := NewDbFindEvent(r)
+	_, err := sut.Find(makeEventFinderOptions())
+	if err == nil {
+		t.Fatal("Expected a fake error")
+	}
+}
